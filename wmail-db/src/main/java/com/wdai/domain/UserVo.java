b2ba55e2b7254b3fa5e9316ebe475f917e3520a4
郑晓龙 <zhengxiaolong@wdai.com>
2019-10-05 00:27:02 +0800

    new project

---
+++ wmail-db/src/main/java/com/wdai/domain/UserVo.java
@@ -0,0 +1,25 @@
+package com.wdai.domain;
+
+/**
+ * Created by WD36713 on 2019/10/4.
+ */
+public class UserVo {
+    private String nickname;
+    private String avatar;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
